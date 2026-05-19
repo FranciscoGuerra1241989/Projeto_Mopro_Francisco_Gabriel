3d5c943bc11484e4eab65701ab750df1cf8039a6
Francisco Guerra <guerrafsilva@gmail.com>
2026-05-19 16:55:18 +0100

    Push template

--- src/main/java/org/example/model/Serie.java
+++ src/main/java/org/example/model/Serie.java
@@ -4,48 +4,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Serie extends Recurso implements MarcavelComoVisto {
-    private List<Temporada> temporadas;
 
-    public Serie(String titulo, int anoLancamento) {
-        super(titulo, anoLancamento);
-        this.temporadas = new ArrayList<>();
-    }
-
-    public List<Temporada> getTemporadas() {
-        return temporadas;
-    }
-
-    public void adicionarTemporada(Temporada temporada) {
-        if (!temporadas.contains(temporada)) {
-            temporadas.add(temporada);
-        }
-    }
-
-    @Override
-    public boolean isVisto(Expectador utilizador) {
-        if (temporadas.isEmpty()) {
-            return false;
-        }
-        for (Temporada t : temporadas) {
-            if (!t.isVisto(utilizador)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public void marcarComoVisto(Expectador utilizador) throws Exception {
-        if (utilizador == null) {
-            throw new Exception("Utilizador inválido");
-        }
-        for (Temporada t : temporadas) {
-            t.marcarComoVisto(utilizador);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "[Série] " + super.toString() + " - Temporadas: " + temporadas.size();
-    }
 }
